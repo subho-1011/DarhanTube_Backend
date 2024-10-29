e3a2da006f96af7854579262cf826196572089f3
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-10-29 19:00:07 +0530

    cors change

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+const allowedOrigins = process.env.CORS_ORIGIN?.split(",") || [];
+
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN,
+        origin: allowedOrigins,
         credentials: true,
     })
 );
